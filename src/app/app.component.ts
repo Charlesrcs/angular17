a96893d627ea9629a688d208362ba4631d6bf98b
Charlesrcs <charlesxaviour0706@gmail.com>
2024-09-21 20:37:09 +0530

    docs file changed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import * as AOS from 'aos';
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -8,6 +8,14 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'angular17';
+  
+ ngOnInit(): void {
+  AOS.init({
+    duration: 1000,  // Duration of animation in milliseconds
+  offset: 120,     // Distance between the bottom of the viewport and the element before animation starts
+  easing: 'ease-in-out',
+  });
+ }
 }
